4646d0e8c93fca78b1362a308d29365f30292634
CoderJuan21 <70552194+CoderJuan21@users.noreply.github.com>
2022-09-19 19:37:47 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const { isObject } = require("util")
+const app = express()
+const server = require("http").Server(app)
+app.set("view engine","ejs")
+app.use(express.static("public"))
+
+const {v4:uuidv4} = require("uuid")
+
+const io = require("socket.io")(server, {
+    cors:{
+        origin:'*'
+    }
+})
+
+app.get("/",(req,res)=>{
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get("/:room",(req,res)=>{
+    res.render("index",{roomId:req.params.room})
+})
+
+// app.get("/", (req,res)=>{
+//     res.status(200).send("Hello Juan")
+// })
+
+io.on("connection", (socket)=>{
+    socket.on("message", (message)=>{
+        io.emit("createMessage", message)
+    })
+})
+server.listen(3030)
